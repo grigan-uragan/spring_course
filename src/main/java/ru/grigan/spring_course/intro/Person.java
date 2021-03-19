050bc9bc7fa9ee676433624f544648ced40450c9
grigan-Uragan <prischepny@mail.ru>
2021-03-19 11:55:55 +0300

    DI with setter

--- src/main/java/ru/grigan/spring_course/intro/Person.java
+++ src/main/java/ru/grigan/spring_course/intro/Person.java
@@ -3,13 +3,17 @@ package ru.grigan.spring_course.intro;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("person bean is created");
-        this.pet = pet;
     }
 
     public void callYourPet() {
         System.out.println("Hello my lovely pet!");
         pet.say();
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("injection pet by setter");
+        this.pet = pet;
     }
 }
