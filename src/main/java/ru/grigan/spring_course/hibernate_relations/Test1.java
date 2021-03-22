de7ff3ff28b2483c071c9162d0c5568a9eac0d91
grigan-Uragan <prischepny@mail.ru>
2021-03-22 17:01:58 +0300

    @OneToOne

---
+++ src/main/java/ru/grigan/spring_course/hibernate_relations/Test1.java
@@ -0,0 +1,42 @@
+package ru.grigan.spring_course.hibernate_relations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.grigan.spring_course.hibernate_relations.entity.Detail;
+import ru.grigan.spring_course.hibernate_relations.entity.Employee;
+
+public class Test1 {
+    private static final Logger LOG = LoggerFactory.getLogger(Test1.class);
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+//            Employee employee = session.get(Employee.class, 1);
+//            System.out.println(employee.getEmpDetail());
+            Employee employee = session.get(Employee.class, 2);
+            System.out.println(employee);
+            session.delete(employee);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            LOG.error("transaction exception", e);
+            factory.getCurrentSession().getTransaction().rollback();
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            factory.close();
+        }
+    }
+}
