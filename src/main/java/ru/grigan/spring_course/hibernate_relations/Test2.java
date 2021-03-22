900777d9e245502a1c30ff3b87ef875197734c87
grigan-Uragan <prischepny@mail.ru>
2021-03-22 18:00:32 +0300

    @OneToOne

---
+++ src/main/java/ru/grigan/spring_course/hibernate_relations/Test2.java
@@ -0,0 +1,47 @@
+package ru.grigan.spring_course.hibernate_relations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.grigan.spring_course.hibernate_relations.entity.Detail;
+import ru.grigan.spring_course.hibernate_relations.entity.Employee;
+
+public class Test2 {
+    private static final Logger LOG = LoggerFactory.getLogger(Test1.class);
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+//            Detail detail = new Detail("Minsk", "12333", "misha@mail.ru");
+//            Employee employee = new Employee("Misha", "Petrov", "Sale", 400);
+//            detail.setEmployee(employee);
+//            employee.setEmpDetail(detail);
+//            session.save(detail);
+//            Detail detail = session.get(Detail.class, 4);
+//            System.out.println(detail.getEmployee());
+            Detail detail = session.get(Detail.class, 4);
+            detail.getEmployee().setEmpDetail(null);
+            session.delete(detail);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            LOG.error("transaction exception", e);
+            factory.getCurrentSession().getTransaction().rollback();
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            factory.close();
+        }
+    }
+}
