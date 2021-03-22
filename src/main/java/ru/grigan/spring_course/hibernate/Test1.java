e2f28825269d968f49346a7c7b383a17194e5fc1
grigan-Uragan <prischepny@mail.ru>
2021-03-22 15:00:00 +0300

    Hibernate mapping employee

---
+++ src/main/java/ru/grigan/spring_course/hibernate/Test1.java
@@ -0,0 +1,33 @@
+package ru.grigan.spring_course.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.grigan.spring_course.hibernate.entity.Employee;
+
+public class Test1 {
+    private static final Logger LOG = LoggerFactory.getLogger(Test1.class);
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            Employee employee = new Employee("Gigan", "Uragan", "IT", 1000);
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            factory.getCurrentSession().getTransaction().rollback();
+            LOG.error("hibernate exception ", e);
+            e.printStackTrace();
+            throw e;
+        } finally {
+            factory.close();
+        }
+    }
+}
